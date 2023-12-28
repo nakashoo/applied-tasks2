9a293f1e7249fd850917c3853536908f2dfed162
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-28 12:38:55 +0000

    <add>追記して元に戻した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
 
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = current_user
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
-  end
 
   def edit
     @user = User.find(params[:id])
