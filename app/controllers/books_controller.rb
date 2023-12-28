7ab0421a1e3f0d15ba88ba2bbf702bc59ae80354
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-28 09:41:25 +0000

    <add>destroyのアクションを追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,11 +35,12 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
+
 
   private
 
