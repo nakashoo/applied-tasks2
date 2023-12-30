fab9fb9646778b9c2f32a0f8ccb38e60024574b1
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-30 07:02:51 +0000

    <add>has_manyにdependent以下の記述を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
-  has_one_attached :profile_image
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  #   validate :custom_name_validation
+
+  # private
+
+  # def custom_name_validation
+  #   if name.blank?
+  #     errors.add(:name, "can't be blank")
+  #   elsif name.length < 2
+  #     errors.add(:name, "is too short (minimum is 2 characters)")
+  #   end
+  # end
+
 end
