e8f88f82b0e3c2a90fd0871bdb1df909407a4666
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-30 09:08:05 +0000

    <add>アクセス制限対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-
+before_action :is_matching_login_user
+before_action :is_matching_login_book,only: [:edit]
 
   def index
     @books = Book.all
@@ -9,8 +10,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = current_user
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book), notice: "You have create book successfully."
     else
       @books = Book.all
       render 'index'
@@ -47,5 +49,20 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title,:body)
   end
+
+   def is_matching_login_user
+     unless user_signed_in?
+     redirect_to new_user_session_path
+     end
+   end
+
+
+  def is_matching_login_book
+    @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 
 end
