6ef271ae88d7f9cc2c5631f9ed33511d26a72671
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-31 01:31:03 +0000

    <add>beforeactionを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
- before_action :is_matching_login_user
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def index
     @users = User.all
     @book = Book.new
-    @user = current_user
   end
 
   def show
@@ -23,9 +22,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(current_user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "edit"
     end
@@ -43,11 +41,4 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-
-  def is_matching_login_user
-    unless user_signed_in?
-     redirect_to new_user_session_path
-    end
-  end
-
 end
