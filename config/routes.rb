fa0873699d633be5e9f8b36b3d84018d054b5d8d
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-31 16:11:03 +0000

    <add>ブックコメントが単数形から複数形に変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   resources :books do
     resource :favorite, only: [:create, :destroy]
-    resources :book_comment, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 end
