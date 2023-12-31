e7a440532693bd429501ed9cb60b8af186e5a302
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-31 11:15:36 +0000

    <add>変数を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:post_image_id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
     redirect_to book_path(book)
@@ -9,7 +9,7 @@ class FavoritesController < ApplicationController
 
 
   def destroy
-    book = Book.find(params[:post_image_id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to book_path(book)
