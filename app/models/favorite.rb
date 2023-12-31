884742dabe7ad193ebd67a062731165272b96beb
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-31 08:34:34 +0000

    <add>バリデーション修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :user_id, uniqueness: {scope: :post_image_id}
+  validates :user_id, uniqueness: {scope: :book_id}
 end
