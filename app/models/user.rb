4abf3edd5879bed14dcde59c882b9a38b546a956
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-01-05 22:22:53 +0000

    <add>フォローとフォロワーのアソシエーションとdefendで３つ定義つけした

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,32 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
-  has_many :subjective_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followerside, through: :subjective_relationships, source: :follower
 
-  has_many :objective_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followedside, through: :objective_elationships, source: :followed
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
+  #中間テーブルを通し、followerモデルのフォローされる側を取得
+  has_many :followed_sides, through: :followers, source: :followed
 
-  has_many :relation, through: :relationships, source: :relationships
-
+  #中間テーブルを通し、followedモデルのフォローする側を取得
+  has_many :follower_sides, through: :followeds, source: :follower
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
 
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+   followed_sides.include?(user)
+  end
 
 
 end
